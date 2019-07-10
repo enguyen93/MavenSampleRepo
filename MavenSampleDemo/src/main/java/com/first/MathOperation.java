f893b53936aa77f25a3a28bf9f271b8a1f53554f
185717 <185717@USTEXPLNDYK4QN2.AMERICAS.GLOBAL.NTTDATA.COM>
2019-07-10 11:52:33 -0500

    secondss

--- MavenSampleDemo/src/main/java/com/first/MathOperation.java
+++ MavenSampleDemo/src/main/java/com/first/MathOperation.java
@@ -21,6 +21,7 @@ public class MathOperation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MathOperation mo = new MathOperation();
+		System.out.println("Addedline");
 		System.out.println("Add: " + mo.add(5, 5));
 		System.out.println("Subtract " + mo.subtract(50, 10));
 		System.out.println("Product " + mo.product(10, 5));
